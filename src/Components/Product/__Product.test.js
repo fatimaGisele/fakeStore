ca0e25471064685193bcc0e74396b297fdccfaf7
fatima <fatima.altamirano@davinci.edu.ar>
2021-08-18 16:45:12 -0300

    update

---
+++ src/Components/Product/__Product.test.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+import Product from './Product';
+
+
+describe("Product", ()=>{
+    test("Should show a product detail", ()=>{
+        
+    })
+})
+// render( <Product/> ); //se encarga de la virtualizacion del componente
+
